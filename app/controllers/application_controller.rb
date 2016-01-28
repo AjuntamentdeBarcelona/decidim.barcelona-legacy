a84af706b4242eced8554105a5ca21f1a9b9f65c
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-01-28 20:03:55 +0100

    Revert "Fix specs"
    This reverts commit f295fba32bc79f0d53eda91010f90a6764d8ec12.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -118,8 +118,6 @@ class ApplicationController < ActionController::Base
     def default_url_options(options={})
       if !Rails.application.secrets.server_name.blank?
         { host: Rails.application.secrets.server_name }
-      else
-        {}
       end
     end
 end
