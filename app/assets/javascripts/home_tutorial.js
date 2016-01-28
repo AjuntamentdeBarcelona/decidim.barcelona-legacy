7f0151fdb8b6c8ced37610b5e57da4d709039bbf
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-01-28 13:25:32 +0100

    Improve animation with maths

--- app/assets/javascripts/home_tutorial.js
+++ app/assets/javascripts/home_tutorial.js
@@ -5,14 +5,40 @@ App.HomeTutorial = {
     if(!$(".home-tutorial")[0]){ return; }
 
     var sr = ScrollReveal();
-    var options = {
-      delay: 100,
-      duration: 600,
-      reset: false
-    };
 
-    sr.reveal(".home-tutorial .icons", options);
-    sr.reveal(".home-tutorial .step-content", options);
-    sr.reveal(".home-tutorial .line-overlay", options);
+    sr.reveal(".home-tutorial .tutorial-step-left .number", {
+      delay: 0,
+      origin: 'left'
+    });
+
+    sr.reveal(".home-tutorial .tutorial-step-right .number", {
+      delay: 0,
+      origin: 'right'
+    });
+
+    sr.reveal(".home-tutorial .tutorial-step-left .icons", {
+      delay: 300,
+      origin: 'right'
+    });
+
+    sr.reveal(".home-tutorial .tutorial-step-right .icons", {
+      delay: 300,
+      origin: 'left'
+    });
+
+    sr.reveal(".home-tutorial .tutorial-step-left .description", {
+      delay: 500,
+      origin: 'right'
+    });
+
+    sr.reveal(".home-tutorial .tutorial-step-right .description", {
+      delay: 500,
+      origin: 'left'
+    });
+
+    sr.reveal(".home-tutorial .line-overlay", {
+      delay: 500,
+      origin: 'top'
+    });
   }
 };
