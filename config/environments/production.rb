df26a8cc9daa94b7749b5ed4c4aafb986bb1321c
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-01-31 11:07:17 +0100

    Revert "Don't verify smtp settings"
    This reverts commit 1ab8f0aeee89270654d6f34e84db52dd46652e3a.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,7 @@ Rails.application.configure do
     :user_name      => Rails.application.secrets.smtp_username,
     :password       => Rails.application.secrets.smtp_password,
     :domain         => Rails.application.secrets.smtp_domain,
-    :enable_starttls_auto => Rails.application.secrets.smtp_starttls_auto,
-    :openssl_verify_mode => 'none'
+    :enable_starttls_auto => Rails.application.secrets.smtp_starttls_auto
   }
 
   if Rails.application.secrets.sendgrid
