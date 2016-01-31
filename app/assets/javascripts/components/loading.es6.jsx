f8aa616082e48f87d7d65acbfcaa2ae7e7b622fd
beagleknight <david.morcillo@gmail.com>
2016-01-31 13:32:40 +0100

    Add jquery events interface to loading component

--- app/assets/javascripts/components/loading.es6.jsx
+++ app/assets/javascripts/components/loading.es6.jsx
@@ -1,6 +1,23 @@
 class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: this.props.show
+    };
+  }
+
+  componentDidMount() {
+    $(document).on('loading:show', () => this.setState({ show: true }));
+    $(document).on('loading:hide', () => this.setState({ show: false }));
+  }
+
+  componentWillUnmount() {
+    $(document).off('loading:show');
+    $(document).off('loading:hide');
+  }
+
   render() {
-    if (this.props.show) {
+    if (this.state.show) {
       return (
         <div className="loading-component"></div>
       )
--- app/assets/javascripts/components/proposal_filters.es6.jsx
+++ app/assets/javascripts/components/proposal_filters.es6.jsx
@@ -10,7 +10,10 @@ class ProposalFilters extends React.Component {
 
     this.filterService = new FilterComponentService(this, {
       requestUrl: this.props.filterUrl,
-      requestDataType: 'script'
+      requestDataType: 'script',
+      onResultsCallback: (result) => {
+        $(document).trigger('loading:hide');
+      }
     });
   }
 
@@ -19,37 +22,37 @@ class ProposalFilters extends React.Component {
       <form className="proposal-filters">
         <SearchFilter 
           searchText={this.state.searchText}
-          onSetFilterText={ (searchText) => this.filterService.setFilterText(searchText) } />
+          onSetFilterText={ (searchText) => this.onSetFilterText(searchText) } />
         <FilterOptionGroup 
           filterGroupName="source" 
           filterGroupValue={this.state.filters.get('source')}
           isExclusive={true}
-          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.filterService.changeFilterGroup(filterGroupName, filterGroupValue) }>
+          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.onChangeFilterGroup(filterGroupName, filterGroupValue) }>
           <FilterOption filterName="official" />
           <FilterOption filterName="citizenship" />
         </FilterOptionGroup>
         <ScopeFilterOptionGroup 
           filterGroupValue={this.state.filters.get('scope')} 
-          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.filterService.changeFilterGroup(filterGroupName, filterGroupValue) } />
+          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.onChangeFilterGroup(filterGroupName, filterGroupValue) } />
         <DistrictFilterOptionGroup 
           scopeSelected={this.state.filters.get('scope')}
           districts={this.props.districts} 
           filterGroupValue={this.state.filters.get('district')}
-          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.filterService.changeFilterGroup(filterGroupName, filterGroupValue) } />
+          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.onChangeFilterGroup(filterGroupName, filterGroupValue) } />
         <CategoryFilterOptionGroup
           categories={this.props.categories}
           filterGroupValue={this.state.filters.get('category_id')} 
-          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.filterService.changeFilterGroup(filterGroupName, filterGroupValue) } />
+          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.onChangeFilterGroup(filterGroupName, filterGroupValue) } />
         <SubcategoryFilterOptionGroup
           selectedCategory={this.state.filters.get('category_id')}
           subcategories={this.props.subcategories}
           filterGroupValue={this.state.filters.get('subcategory_id')}
-          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.filterService.changeFilterGroup(filterGroupName, filterGroupValue) } />
+          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.onChangeFilterGroup(filterGroupName, filterGroupValue) } />
         <FilterOptionGroup 
           filterGroupName="other" 
           filterGroupValue={this.state.filters.get('other')}
           isExclusive={true}
-          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.filterService.changeFilterGroup(filterGroupName, filterGroupValue) }>
+          onChangeFilterGroup={(filterGroupName, filterGroupValue) => this.onChangeFilterGroup(filterGroupName, filterGroupValue) }>
           <FilterOption filterName="meetings" />
         </FilterOptionGroup>
         {this.renderTagCloudFilter()}
@@ -63,9 +66,24 @@ class ProposalFilters extends React.Component {
         <TagCloudFilter 
           currentTags={this.state.tags} 
           tagCloud={this.props.filter.tag_cloud} 
-          onSetFilterTags={(tags) => this.filterService.setFilterTags(tags)} />
+          onSetFilterTags={(tags) => this.onSetFilterTags(tags)} />
       )
     }
     return null;
+  }
+
+  onSetFilterText(searchText) {
+    $(document).trigger('loading:show');
+    this.filterService.setFilterText(searchText);
+  }
+
+  onChangeFilterGroup(filterGroupName, filterGroupValue) {
+    $(document).trigger('loading:show');
+    this.filterService.changeFilterGroup(filterGroupName, filterGroupValue);
+  }
+
+  onSetFilterTags(tags) {
+    $(document).trigger('loading:show');
+    this.filterService.setFilterTags(tags);
   }
 }
