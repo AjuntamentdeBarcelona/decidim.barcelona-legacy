ae684cc17f4edd2656cebee5964eebea85d85214
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-01-31 09:24:24 +0100

    Add puma worker killer

--- config/puma.rb
+++ config/puma.rb
@@ -8,6 +8,18 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+before_fork do
+  if defined?(PumaWorkerKiller)
+    PumaWorkerKiller.config do |config|
+      config.ram           = 1024 # mb
+      config.frequency     = 5    # seconds
+      config.percent_usage = 0.98
+      config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
+    end
+    PumaWorkerKiller.start
+  end
+end
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
