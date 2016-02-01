b4aa70291b8e40e51ea18a0123feec9bf43acfd3
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-02-01 10:48:50 +0100

    Parameterize downloads

--- app/helpers/downloads_helper.rb
+++ app/helpers/downloads_helper.rb
@@ -1,17 +1,24 @@
 module DownloadsHelper
   def download_for(district)
+    langs = {
+      es: "CAST",
+      ca: "CAT"
+    }
+
+    lang = langs[I18n.locale]
+
     case district 
-      when "1"  then "http://www.barcelona.cat/download/pam/ca/PAD-Ciutat-Vella-CAT.pdf"
-      when "2"  then "http://www.barcelona.cat/download/pam/ca/PAD-Eixample-CAT.pdf"
-      when "3"  then "http://www.barcelona.cat/download/pam/ca/PAD-Sants-Montjuic-CAT.pdf"
-      when "4"  then "http://www.barcelona.cat/download/pam/ca/PAD-Les-Corts-CAT.pdf"
-      when "5"  then "http://www.barcelona.cat/download/pam/ca/PAD-Sarria-CAT.pdf"
-      when "6"  then "http://www.barcelona.cat/download/pam/ca/PAD-Gracia-CAT.pdf"
-      when "7"  then "http://www.barcelona.cat/download/pam/ca/PAD-Horta-Guinardo-CAT.pdf"
-      when "8"  then "http://www.barcelona.cat/download/pam/ca/PAD-Nou-Barris-CAT.pdf"
-      when "9"  then "http://www.barcelona.cat/download/pam/ca/PAD-Sant-Andreu-CAT.pdf" 
-      when "10" then "http://www.barcelona.cat/download/pam/ca/PAD-Sant-Marti-CAT.pdf"
-      else "http://www.barcelona.cat/download/pam/ca/Document-PAM-2016.pdf"
+      when "1"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Ciutat-Vella-#{lang}.pdf"
+      when "2"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Eixample-#{lang}.pdf"
+      when "3"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Sants-Montjuic-#{lang}.pdf"
+      when "4"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Les-Corts-#{lang}.pdf"
+      when "5"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Sarria-#{lang}.pdf"
+      when "6"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Gracia-#{lang}.pdf"
+      when "7"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Horta-Guinardo-#{lang}.pdf"
+      when "8"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Nou-Barris-#{lang}.pdf"
+      when "9"  then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Sant-Andreu-#{lang}.pdf" 
+      when "10" then "http://www.barcelona.cat/download/pam/#{I18n.locale}/PAD-Sant-Marti-#{lang}.pdf"
+      else "http://www.barcelona.cat/download/pam/#{I18n.locale}/Document-PAM-2016.pdf"
     end
   end
 end
