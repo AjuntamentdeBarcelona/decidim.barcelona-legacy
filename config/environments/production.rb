ebe308eaf390492ed93a7fa8871af05c97209394
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-02-01 11:44:00 +0100

    Fix asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//#{Rails.application.secrets.server_name}"
+  config.action_controller.asset_host = "https://#{Rails.application.secrets.server_name}"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
