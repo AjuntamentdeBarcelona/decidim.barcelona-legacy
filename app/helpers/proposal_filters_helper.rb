7a849263eef946035a6b8bfc3ef16d52032af7f9
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-02-01 16:44:07 +0100

    Fix proposals index

--- app/helpers/proposal_filters_helper.rb
+++ app/helpers/proposal_filters_helper.rb
@@ -2,12 +2,21 @@ module ProposalFiltersHelper
   def proposal_filters(options = {})
     react_component(
       'ProposalFilters', 
-      filter: options[:filter],
+      filter: serialized_filters(options[:filter]),
       filterUrl: proposals_url,
       districts: Proposal::DISTRICTS,
       categories: serialized_categories,
       subcategories: serialized_subcategories,
       tagsEnabled: feature?(:proposal_tags)
     ) 
+  end
+
+  def serialized_filters(filters)
+    {
+      search_filter: filters.search_filter,
+      tag_filter: filters.tag_filter,
+      params: filters.params,
+      tag_cloud: filters.tag_cloud
+    }
   end
 end
