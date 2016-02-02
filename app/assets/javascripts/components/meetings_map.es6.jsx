632bdd2d37062314b95881f8d67af1cf592e61a0
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-02-02 22:25:21 +0100

    Fix map center

--- app/assets/javascripts/components/meetings_map.es6.jsx
+++ app/assets/javascripts/components/meetings_map.es6.jsx
@@ -2,7 +2,7 @@ class MeetingsMap extends React.Component {
   constructor(props) {
     super(props);
     this.defaultZoom = 12;
-    this.defaultLocation = { lat: 41.3947688, lng: 2.078728 };
+    this.defaultLocation = { lat: 41.396449, lng: 2.169971 };
     this.currentInfoWindow = null;
   }
 
