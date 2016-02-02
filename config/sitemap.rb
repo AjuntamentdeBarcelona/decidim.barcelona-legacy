96fa60979bda5bbd9256e8865f6edbf76c55fdf4
beagleknight <david.morcillo@gmail.com>
2016-02-02 11:43:57 +0100

    Add dynamic and static pages to sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,12 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "#{!ENV["FORCE_SSL"].blank? ? "https" : "http"}://#{ENV["SERVER_NAME"]}"
 
-SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+SitemapGenerator::Sitemap.create(verbose: true) do
+  # Add meetings pages
+  Meeting.all.each { |meeting| add meeting_path(meeting), lastmod: meeting.updated_at }
+  # Add proposals pages
+  Proposal.all.each { |proposal| add proposal_path(proposal), lastmod: proposal.updated_at }
+  # Add static pages
+  add categories_path
+  add page_path('more_information')
 end
