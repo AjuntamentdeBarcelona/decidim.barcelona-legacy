b3a0c5f070da7b1b49c0ffbc289a22f835c706ef
beagleknight <david.morcillo@gmail.com>
2016-02-02 12:17:36 +0100

    Use inner class to handle sitemap write

--- lib/sitemap_middleware.rb
+++ lib/sitemap_middleware.rb
@@ -5,33 +5,46 @@ class SitemapMiddleware
 
   def call(env)
     if env['PATH_INFO'] == '/sitemap.xml'
-      generate_sitemap
       response = Rack::Response.new
       response.header['Content-Type'] = 'text/xml'
-      response.body = [@raw_data]
+      response.body = [generate_sitemap]
       response.finish
     else
       @app.call(env)
     end
   end
 
-  def write(location, raw_data)
-    @raw_data = raw_data
-  end
-
   private
 
+  class SitemapWriter
+    def initialize
+      @stream = StringIO.new
+    end
+
+    def to_s
+      @stream.string
+    end
+
+    def write(location, raw_xml)
+      @stream.write(raw_xml)
+    end
+  end
+
   def generate_sitemap
+    writer = SitemapWriter.new
+
     SitemapGenerator::Sitemap.default_host = "#{!ENV["FORCE_SSL"].blank? ? "https" : "http"}://#{ENV["SERVER_NAME"]}"
 
-    SitemapGenerator::Sitemap.create(adapter: self) do
+    SitemapGenerator::Sitemap.create(adapter: writer) do
       # Add meetings pages
-      Meeting.all.each { |meeting| add meeting_path(meeting), lastmod: meeting.updated_at }
+      Meeting.find_each { |meeting| add meeting_path(meeting), lastmod: meeting.updated_at }
       # Add proposals pages
-      Proposal.all.each { |proposal| add proposal_path(proposal), lastmod: proposal.updated_at }
+      Proposal.find_each { |proposal| add proposal_path(proposal), lastmod: proposal.updated_at }
       # Add static pages
       add categories_path
       add page_path('more_information')
     end
+
+    writer.to_s
   end
 end
