1ebf55046a23589cb9ebba4b48ac5ffe6f15d39b
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-02-05 12:55:17 +0100

    Add notifications reminder

---
+++ app/controllers/email_notifications_reminders_controller.rb
@@ -0,0 +1,18 @@
+class EmailNotificationsRemindersController < ApplicationController
+  skip_authorization_check
+
+  def create
+    current_user.update_attributes(
+      weekly_summary: true,
+      email_on_comment: true,
+      email_on_comment_reply: true
+    )
+
+    destroy
+  end
+
+  def destroy
+    $redis.srem("email_notifications_reminder", current_user.id.to_s)
+    render nothing: true
+  end
+end
