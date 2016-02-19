bd4950ed4836abdfcd9354fe55857497d124011d
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-02-19 20:27:16 +0100

    Cache categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
 
   def index
     categories = Category.includes(:subcategories)
+    @last_modified = [Category.maximum(:updated_at), Subcategory.maximum(:updated_at)].max
     @categories ||= CategoryDecorator.decorate_collection(categories)
   end
 end
