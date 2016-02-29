8e7074739d2905df3e0e8457319d4f6ec5244fb2
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-02-29 12:43:30 +0100

    Allow document number to be blank instead of just nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, if: :username_required?
   validates :username, uniqueness: true, if: :username_required?
-  validates :document_number, uniqueness: { scope: :document_type }, allow_nil: true
+  validates :document_number, uniqueness: { scope: :document_type }, allow_blank: true
 
   validate :validate_username_length
 
