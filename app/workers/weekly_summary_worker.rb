b68e92479600bd3dc4217acc946320e23ca2edaa
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-03-02 15:46:13 +0100

    Deliver emails later

--- app/workers/weekly_summary_worker.rb
+++ app/workers/weekly_summary_worker.rb
@@ -6,7 +6,7 @@ class WeeklySummaryWorker
     to = Time.now
 
     User.where(weekly_summary: true).find_each do |user|
-      SummaryMailer.weekly(user, from, to).deliver
+      SummaryMailer.weekly(user, from, to).deliver_later
     end
 
     time = Time.now.next_week(:monday).to_datetime.change({ hour: 7, min: 30, sec: 0 })
