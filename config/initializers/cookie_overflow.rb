fa198fc63af25417033ff53a770e45c050eeb084
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-03-12 21:34:30 +0100

    Prevent cookie overflow

---
+++ config/initializers/cookie_overflow.rb
@@ -0,0 +1,11 @@
+# This tries to address cookie overflow exceptions.
+module SafeStoreLocation
+
+  MAX_LOCATION_SIZE = ActionDispatch::Cookies::MAX_COOKIE_SIZE / 2
+
+  def store_location_for(resource_or_scope, location)
+    super unless location && location.size > MAX_LOCATION_SIZE
+  end
+end
+
+Devise::FailureApp.include SafeStoreLocation
