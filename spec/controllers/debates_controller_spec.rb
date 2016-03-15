fa71f174083c6f808be1ca44422548340fb7b373
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-03-15 16:28:22 +0100

    Remove test

--- spec/controllers/debates_controller_spec.rb
+++ spec/controllers/debates_controller_spec.rb
@@ -6,17 +6,6 @@ describe DebatesController do
     Setting['feature.debates'] = true
   end
 
-  describe 'POST create' do
-    it 'should create an ahoy event' do
-
-      sign_in create(:user, :official)
-
-      post :create, debate: { title: 'A sample debate', description: 'this is a sample debate', terms_of_service: 1 }
-      expect(Ahoy::Event.where(name: :debate_created).count).to eq 1
-      expect(Ahoy::Event.last.properties['debate_id']).to eq Debate.last.id
-    end
-  end
-
   describe "Vote with too many anonymous votes" do
     it 'should allow vote if user is allowed' do
       Setting["max_ratio_anon_votes_on_debates"] = 100
