55435072614e7365cdae071cec56de308f3adb20
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-03-15 16:42:27 +0100

    FIX MIGRATION... *allegedly*

--- db/migrate/20160315153317_prevent_null_datetime_in_debates.rb
+++ db/migrate/20160315153317_prevent_null_datetime_in_debates.rb
@@ -1,6 +1,6 @@
 class PreventNullDatetimeInDebates < ActiveRecord::Migration
   def change
-    Debate.update_all('starts_at = created_at, ends_at = created_at')
+    Debate.with_hidden.update_all('starts_at = created_at, ends_at = created_at')
     change_column :debates, :starts_at, :datetime, null: false
     change_column :debates, :ends_at, :datetime, null: false
   end
