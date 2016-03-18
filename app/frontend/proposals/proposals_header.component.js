4a59ff718cf3bf13a96212b7936c99b0e66da5a8
beagleknight <david.morcillo@gmail.com>
2016-03-18 08:42:50 +0100

    Restore proposals headers

--- app/frontend/proposals/proposals_header.component.js
+++ app/frontend/proposals/proposals_header.component.js
@@ -3,16 +3,16 @@ export default function () {
     <div className="page-title">
       <div className="wrap row proposals-header">
         <div className="small-12 columns">
-          <h2>Title</h2>
+          <h2>{ I18n.t('proposals.index.title') }</h2>
         </div>
       </div>
       <div className="wrap row proposals-header">
         <div className="small-12 medium-7 large-9 columns">
-          <h3>Description</h3>
+          <h3>{ I18n.t('proposals.index.description') }</h3>
         </div>
         <div className="small-12 medium-5 large-3 columns">
           <div className="download-buttons">
-            <a><span>Download</span></a>
+            <a href="/download" className="download-pam"><span>{ I18n.t('proposals.index.download') }</span></a>
           </div>
         </div>
       </div>
