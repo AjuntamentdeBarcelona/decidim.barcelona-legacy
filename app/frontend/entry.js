1701ca68084be3ce7911228a558737187ad604b2
beagleknight <david.morcillo@gmail.com>
2016-03-18 08:18:22 +0100

    Restore votes component and organize frontend folder

--- app/frontend/entry.js
+++ app/frontend/entry.js
@@ -1,21 +1,22 @@
 require('expose?React!react');
 require('expose?ReactDOM!react-dom');
 
-import Loading                    from './javascripts/application/loading.component';
-import CookiesWarning             from './javascripts/application/cookies_warning.component';
-import EmailNotificationsReminder from './javascripts/application/email_notifications_reminder.component';
-import RichEditor                 from './javascripts/application/rich_editor.component';
-import StaticMap                  from './javascripts/location/static_map.component';
-import AutocompleteInputAddress   from './javascripts/location/autocomplete_input_address.component';
-import FilterService              from './javascripts/filters/filter.service';
-import CategoryPicker             from './javascripts/categories/category_picker.component';
-import ProposalsCarousel          from './javascripts/proposals/proposals_carousel.component';
-import MeetingsDirectory          from './javascripts/meetings/meetings_directory.component';
-import MeetingsCarousel           from './javascripts/meetings/meetings_carousel.component';
-import MeetingsMap                from './javascripts/meetings/meetings_map.component';
-import MeetingProposalsSelector   from './javascripts/meetings/meeting_proposals_selector.component';
+import Loading                    from './application/loading.component';
+import CookiesWarning             from './application/cookies_warning.component';
+import EmailNotificationsReminder from './application/email_notifications_reminder.component';
+import RichEditor                 from './application/rich_editor.component';
+import StaticMap                  from './location/static_map.component';
+import AutocompleteInputAddress   from './location/autocomplete_input_address.component';
+import FilterService              from './filters/filter.service';
+import CategoryPicker             from './categories/category_picker.component';
+import ProposalsCarousel          from './proposals/proposals_carousel.component';
+import MeetingsDirectory          from './meetings/meetings_directory.component';
+import MeetingsCarousel           from './meetings/meetings_carousel.component';
+import MeetingsMap                from './meetings/meetings_map.component';
+import MeetingProposalsSelector   from './meetings/meeting_proposals_selector.component';
 import DebateInfoBox              from './javascripts/debates/debate_info_box.component';
-import ProposalsApp               from './javascripts/proposals/proposals_app.component';
+import ProposalsApp               from './proposals/proposals_app.component';
+import Votes                      from './proposals/votes.component'; // Deprecated
 
 window.Loading                    = Loading;
 window.CookiesWarning             = CookiesWarning;
@@ -32,6 +33,7 @@ window.MeetingsMap                = MeetingsMap;
 window.MeetingProposalsSelector   = MeetingProposalsSelector;
 window.DebateInfoBox              = DebateInfoBox;
 window.ProposalsApp               = ProposalsApp;
+window.Votes                      = Votes; // Depcrecated
 
 require('quill/dist/quill.snow');
 
