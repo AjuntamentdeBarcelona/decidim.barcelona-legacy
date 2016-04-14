722007283668bfcda63bcb67af8e579a330c552c
beagleknight <david.morcillo@gmail.com>
2016-04-14 14:35:12 +0200

    Fix broken tests

--- spec/models/abilities/reviewer_spec.rb
+++ spec/models/abilities/reviewer_spec.rb
@@ -5,5 +5,5 @@ describe "Abilities::Reviewer" do
   subject(:ability) { Ability.new(user) }
   let(:user) { create(:user, roles: [:reviewer]) }
 
-  it { should be_able_to(:manage, Action) }
+  it { should be_able_to(:manage, ActionPlan) }
 end
