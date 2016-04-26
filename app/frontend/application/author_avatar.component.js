1f25d13f4a41d16609cb3306cfc6099ecd318356
beagleknight <david.morcillo@gmail.com>
2016-04-26 10:03:11 +0200

    Add dynamic background color to avatars

--- app/frontend/application/author_avatar.component.js
+++ app/frontend/application/author_avatar.component.js
@@ -1,9 +1,20 @@
+import Please from 'pleasejs';
+
+const MAX_COLORS = 26;
+const COLORS = Please.make_color({
+  colors_returned: MAX_COLORS
+});
+const FIRST_LETTER_CHAR_CODE = 'A'.charCodeAt();
+
 export default ({
   author
 }) => {
   const firstLetter = author.name[0].toUpperCase();
+  const style = {
+    backgroundColor: COLORS[(firstLetter.charCodeAt() - FIRST_LETTER_CHAR_CODE) % MAX_COLORS]
+  };
   
   return (
-    <span className="author-avatar left">{firstLetter}</span>
+    <span style={style} className="author-avatar left">{firstLetter}</span>
   );
 }
