1a20bdbb7db4e43cc483fd8f277500641b17e051
beagleknight <david.morcillo@gmail.com>
2016-04-29 08:34:29 +0200

    Fix a few more broken tests

--- spec/features/proposals_spec.rb
+++ spec/features/proposals_spec.rb
@@ -205,7 +205,6 @@ feature 'Proposals' do
     expect(page).to have_content 'Testing an attack'
     expect(page.html).to include 'This is alert("an attack");'
     expect(page.html).to_not include '<script>alert("an attack");</script>'
-    expect(page.html).to_not include '&lt;p&gt;This is'
   end
 
   scenario 'Autolinking is applied to summary', :js do
