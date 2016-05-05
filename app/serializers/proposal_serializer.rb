4f71954d434cab5500685a326827b9720dc8f45e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-05-05 15:23:59 +0200

    Add comments count

--- app/serializers/proposal_serializer.rb
+++ app/serializers/proposal_serializer.rb
@@ -1,8 +1,10 @@
 class ProposalSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :summary, :created_at, :scope_, :district, :source, 
-    :total_votes, :total_comments, :voted, :votable, :closed, :official, :from_meeting,
-    :editable, :conflictive?, :external_url, :hidden?, :can_hide, :can_hide_author,
-    :flagged, :code
+  attributes :id, :title, :url, :summary, :created_at, :scope_, :district,
+             :source, :total_votes, :total_comments, :total_positive_comments,
+             :total_negative_comments, :total_neutral_comments, :voted,
+             :votable, :closed, :official, :from_meeting, :editable,
+             :conflictive?, :external_url, :hidden?, :can_hide, :can_hide_author,
+             :flagged, :code
 
   has_one :category
   has_one :subcategory
@@ -22,6 +24,18 @@ class ProposalSerializer < ActiveModel::Serializer
 
   def total_comments
     object.comments.count
+  end
+
+  def total_positive_comments
+    object.comments.positive.count
+  end
+
+  def total_negative_comments
+    object.comments.negative.count
+  end
+
+  def total_neutral_comments
+    object.comments.neutral.count
   end
 
   def voted
