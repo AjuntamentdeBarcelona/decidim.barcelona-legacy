fa30bf587d5154fb5d04c9837c9ca80624a61ed1
beagleknight <david.morcillo@gmail.com>
2016-06-06 10:42:56 +0200

    Add placeholder data for action plan statistics

---
+++ app/frontend/action_plans/action_plan_statistics.component.js
@@ -0,0 +1,16 @@
+import {Component} from 'react';
+
+export default class ActionPlanStatistics extends Component {
+  render() {
+    return (
+      <ul>
+        <li><span>30</span> Propostes relacionades</li>
+        <li><span>25</span> Suma de suports</li>
+        <li><span>15</span> Suma de comentaris</li>
+        <li><span>25</span> Suma de participants presencials</li>
+        <li><span>30</span> Intervencions en cites</li>
+        <li><span>60</span> Intervencions totals</li>
+      </ul>
+    );
+  }
+}
