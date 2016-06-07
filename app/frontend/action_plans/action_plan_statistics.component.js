091a0abfcddb172f48c6abfa9e486366236e7b63
beagleknight <david.morcillo@gmail.com>
2016-06-07 09:43:58 +0200

    DRYed statistics component a bit

--- app/frontend/action_plans/action_plan_statistics.component.js
+++ app/frontend/action_plans/action_plan_statistics.component.js
@@ -2,29 +2,21 @@ export default ({
   statistics
 }) => (
   <ul>
-    <li>
-      <span className="number">{statistics.related_proposals_count}</span>
-      {I18n.t('components.action_plan_statistics.related_proposals_count')}
-    </li>
-    <li>
-      <span className="number">{statistics.supports_count}</span>
-      {I18n.t('components.action_plan_statistics.supports_count')}
-    </li>
-    <li>
-      <span className="number">{statistics.comments_count}</span>
-      {I18n.t('components.action_plan_statistics.comments_count')}
-    </li>
-    <li>
-      <span className="number">{statistics.participants_count}</span>
-      {I18n.t('components.action_plan_statistics.participants_count')}
-    </li>
-    <li>
-      <span className="number">{statistics.meeting_interventions_count}</span>
-      {I18n.t('components.action_plan_statistics.meeting_interventions_count')}
-    </li>
-    <li>
-      <span className="number">{statistics.interventions_count}</span>
-      {I18n.t('components.action_plan_statistics.interventions_count')}
-    </li>
+    {renderStatistics(statistics)}
   </ul>
 );
+
+function renderStatistics (statistics) {
+  let result = [];
+
+  for (let key in statistics) {
+    result.push(
+      <li key={key}>
+        <span className="number">{statistics[key]}</span>
+        {I18n.t(`components.action_plan_statistics.${key}`)}
+      </li>
+    );
+  }
+
+  return result;
+}
