4aa9061a9a1051c503dffa6b6c13f8a55dcc2538
beagleknight <david.morcillo@gmail.com>
2016-06-08 12:01:19 +0200

    Improve proposals api performance

--- app/controllers/api/proposals_controller.rb
+++ app/controllers/api/proposals_controller.rb
@@ -13,6 +13,11 @@ class Api::ProposalsController < Api::ApplicationController
 
     proposals = @current_order == "recommended" ? Recommender.new(current_user).proposals : Proposal.all
 
+    proposals = proposals
+    .includes(:comments)
+    .includes(:flags)
+    .includes(:votes_for)
+
     @proposals = ResourceFilter.new(params, user: current_user)
       .filter_collection(proposals.includes(:category, :subcategory, :author => [:organization]))
       .send("sort_by_#{@current_order}")
