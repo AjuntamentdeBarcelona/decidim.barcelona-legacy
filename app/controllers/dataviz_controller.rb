21ff69e8efb0915a97e1e5edb9181c18ce3d6833
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-07-27 10:46:16 +0200

    Add downloads

--- app/controllers/dataviz_controller.rb
+++ app/controllers/dataviz_controller.rb
@@ -14,6 +14,6 @@ class DatavizController < ApplicationController
   private
 
   def available_dataviz
-    ["summary", "total_interactions", "proposals", "meetings", "map"]
+    ["summary", "total_interactions", "proposals", "meetings", "map", "open_data"]
   end
 end
