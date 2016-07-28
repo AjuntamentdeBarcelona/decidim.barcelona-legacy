1b2acf12ee57ac33972fe6116ea8c7c61ff66a4e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-07-28 17:38:23 +0200

    Fix specs

--- spec/mailers/proposal_results_mailer_spec.rb
+++ spec/mailers/proposal_results_mailer_spec.rb
@@ -3,36 +3,38 @@ require 'rails_helper'
 describe ProposalResultsMailer do
   describe '#user_summary' do
     let(:user) { create(:user) }
-    let!(:authored_accepted_proposal) {
-      answer = build(:proposal_answer, status: 'accepted', message: "Reason 1.")
-      create(:proposal, author: user, answer: answer)
-    }
+    let!(:authored_accepted_proposal) do
+      answer = build(:proposal_answer, status: 'accepted')
+      create(:proposal, author: user, answer: answer, title: 'Proposal 1.')
+    end
 
-    let!(:authored_rejected_proposal) {
-      answer = build(:proposal_answer, status: 'rejected', message: "Reason 2")
-      create(:proposal, author: user, answer: answer)
-    }
+    let!(:authored_rejected_proposal) do
+      answer = build(:proposal_answer, status: 'rejected')
+      create(:proposal, author: user, answer: answer, title: 'Proposal 2')
+    end
 
-    let!(:followed_accepted_proposal) {
-      answer = build(:proposal_answer, status: 'accepted', message: "Reason 3")
-      create(:proposal, author: user, answer: answer).tap do |proposal|
+    let!(:followed_accepted_proposal) do
+      answer = build(:proposal_answer, status: 'accepted')
+      create(:proposal, author: user, answer: answer, title: 'Proposal 3').tap do |proposal|
         Follow.create(follower: user, following: proposal)
       end
-    }
+    end
 
-    let!(:followed_rejected_proposal) {
-      answer = build(:proposal_answer, status: 'rejected', message: "Reason 4")
-      create(:proposal, author: user, answer: answer).tap do |proposal|
+    let!(:followed_rejected_proposal) do
+      answer = build(:proposal_answer, status: 'rejected')
+      create(:proposal, author: user, answer: answer, title: 'Proposal 4').tap do |proposal|
         Follow.create(follower: user, following: proposal)
       end
-    }
+    end
+
+    let!(:random_proposal) { create(:proposal, title: 'Proposal 5') }
 
     it 'renders proposals and their answers' do
       email = ProposalResultsMailer.user_summary(user)
       content = mail_content(email)
 
-      expect(content).to include('Reason 2', 'Reason 4')
-      expect(content).to_not include('Reason 1', 'Reason 3')
+      expect(content).to include('Proposal 1', 'Proposal 2', 'Proposal 3', 'Proposal 4')
+      expect(content).to_not include('Proposal 5')
     end
   end
 end
