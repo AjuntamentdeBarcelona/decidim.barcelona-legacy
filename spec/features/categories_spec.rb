983ae31953f96da9735d466b98fe6bab51710f11
beagleknight <david.morcillo@gmail.com>
2016-09-19 12:27:00 +0200

    Fix more broken tests

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 feature 'Categories' do
-  let!(:subcategory1){ create(:subcategory) }
-  let!(:subcategory2){ create(:subcategory) }
-  let(:category1){ subcategory1.category }
-  let(:category2){ subcategory2.category }
+  let!(:participatory_process) { create(:participatory_process) }
+  let!(:category1){ create(:category, participatory_process_id: participatory_process.id) }
+  let!(:category2){ create(:category, participatory_process_id: participatory_process.id) }
+  let!(:subcategory1){ create(:subcategory, category_id: category1.id, participatory_process_id: participatory_process.id) }
+  let!(:subcategory2){ create(:subcategory, category_id: category2.id,  participatory_process_id: participatory_process.id) }
 
   scenario "Shows all the categories and subcategories" do
-    visit categories_path
+    visit categories_path(participatory_process_id: participatory_process)
 
     [category1, category2].each do |category| 
       expect(page).to have_content category.name["en"]
