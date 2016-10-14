35ceda5b506e3f0c2f065ec28e212367c7081154
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-10-14 12:21:29 +0200

    Remove vote box from proposals for now

--- app/frontend/proposals/proposal.component.js
+++ app/frontend/proposals/proposal.component.js
@@ -1,6 +1,5 @@
 import FilterMeta      from '../filters/filter_meta.component';
 
-import ProposalVoteBox from './proposal_vote_box.component';
 import ProposalInfo    from './proposal_info.component';
 
 import htmlToReact from '../application/html_to_react';
@@ -9,53 +8,33 @@ import simpleFormat from '../application/simple_format';
 const Proposal = (proposal) => (
   <div id={`proposal_${proposal.id}`} className="proposal column">
     <article className="card card--proposal">
+      
       <div className="card__content">
-        <div className="proposal-content">
+        <div className="card__header">
           <a href={proposal.url} className="card__link">
             <h5 className="card__title">{ proposal.title }</h5>
           </a>
           <ProposalInfo 
-            code={ proposal.code }
             created_at={ proposal.created_at }
-            status={ proposal.status }
-            official={ proposal.official }
-            from_meeting={ proposal.from_meeting }
             author={ proposal.author }/>
-          <p className="proposal-description">
-            {(() => {
-              if (proposal.status) {
-                return (
-                    <span className="proposal-status badge">
-                    <span className="bullet">&nbsp;&bull;&nbsp;</span>
-                    <span className={proposal.status}>{I18n.t(`proposals.status.${proposal.status}`)}</span>
-                    </span>
-                );
-              } else {
-                return null;
-              }
-            })()}
-            {htmlToReact(simpleFormat(proposal.summary))}
-          </p>
-          <div className="bottom-bar">
-            <FilterMeta 
-              scope={ proposal.scope_ }
-              district={ proposal.district }
-              category={ proposal.category }
-              subcategory={ proposal.subcategory } />
-          </div>
+        </div>
+        <p>
+          {htmlToReact(simpleFormat(proposal.summary))}
+        </p>
+        <FilterMeta 
+          scope={ proposal.scope_ }
+          district={ proposal.district }
+          category={ proposal.category }
+          subcategory={ proposal.subcategory } />
+      </div>
+      <div className="card__footer">
+        <div className="card__support">
+          <div className="card__support__data"></div>
+          <a href={proposal.url} className="card__button button small secondary">
+            {I18n.t('proposals.edit.show_link')}
+          </a>
         </div>
       </div>
-      <aside id={`proposal_${proposal.id}_votes`} className="card__footer">
-        <ProposalVoteBox 
-          hideButton={ proposal.closed }
-          proposalId={ proposal.id } 
-          proposalTitle={ proposal.title } 
-          proposalUrl={ proposal.url } 
-          voted={ proposal.voted } 
-          votable={ proposal.votable } 
-          totalVotes={ proposal.total_votes } 
-          totalComments={ proposal.total_comments } />
-      </aside>
     </article>
   </div>
 );
