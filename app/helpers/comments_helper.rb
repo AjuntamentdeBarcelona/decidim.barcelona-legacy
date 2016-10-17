6eaf5ba73d3de95479de2cb14be3ee22b0d03c6e
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-10-17 19:54:34 +0200

    Fix commentable helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,10 @@
 module CommentsHelper
   def commentable_path(comment)
-    send("#{comment.commentable.class.to_s.downcase}_path", comment.commentable, participatory_process_id: comment.commentable.participatory_process)
+    participatory_process = comment.commentable.participatory_process
+    name = comment.commentable.class.name.downcase.pluralize
+
+    send("#{comment.commentable.class.to_s.downcase}_path", comment.commentable,
+         participatory_process_id: participatory_process,
+         step_id: Step.step_for(participatory_process, name))
   end
 end
