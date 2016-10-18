e036bdc9276f9d550f1cf5f3961e3ec940dc83bb
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-10-18 16:22:52 +0200

    Add participatory process show

--- app/decorators/participatory_process_decorator.rb
+++ app/decorators/participatory_process_decorator.rb
@@ -2,4 +2,14 @@ class ParticipatoryProcessDecorator < ApplicationDecorator
   delegate_all
 
   translates :title, :subtitle, :summary, :description
+
+  decorates_association :steps
+
+  def scope
+    if object.scope == "city"
+      h.t("scopes.city")
+    elsif object.district
+      District.find(object.district).name
+    end
+  end
 end
