f62c0c4b44e844eafd2551de2a60c376123f0087
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-10-18 13:04:49 +0200

    Fix lint errors

--- app/frontend/action_plans/action_plan.component.js
+++ app/frontend/action_plans/action_plan.component.js
@@ -1,8 +1,6 @@
 import { PropTypes }        from 'react';
 import ellipsis             from 'html-ellipsis';
 
-import ActionPlanStatistics from './action_plan_statistics.component';
-import SocialShareButtons   from '../application/social_share_buttons.component';
 import FilterMeta           from '../filters/filter_meta.component';
 
 import htmlToReact          from '../application/html_to_react';
@@ -14,7 +12,6 @@ const ActionPlan = ({
   url,
   title,
   description,
-  created_at,
   category,
   subcategory,
   scope_,
@@ -51,22 +48,6 @@ const ActionPlan = ({
       </div>
     </article>
   </div>
-
-
-  //         <div className="bottom-bar">
-  //           
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <aside id={`action_plan_${id}_statistics`} className="actionplan-statistics small-12 medium-4 column">
-  //       <div className="action-plan-statistics-inner">
-  //         <ActionPlanStatistics statistics={statistics}></ActionPlanStatistics>
-  //         <hr></hr>
-  //         <SocialShareButtons title={title} url={url} />
-  //       </div>
-  //     </aside>
-  //   </div>
-  // </div>
 );
 
 ActionPlan.propTypes = {
@@ -74,7 +55,6 @@ ActionPlan.propTypes = {
   url: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  created_at: PropTypes.string.isRequired,
   category: PropTypes.object.isRequired,
   subcategory: PropTypes.object.isRequired,
   scope_: PropTypes.string.isRequired,
