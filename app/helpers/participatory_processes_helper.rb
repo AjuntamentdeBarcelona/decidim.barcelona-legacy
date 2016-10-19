26c76e5bb4cec23a8ac2f3661864d1c4a98abc84
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-10-19 17:07:58 +0200

    Fix category picker for proposal#form

--- app/helpers/participatory_processes_helper.rb
+++ app/helpers/participatory_processes_helper.rb
@@ -4,6 +4,6 @@ module ParticipatoryProcessesHelper
       [(p.title.present? && p.title[I18n.locale.to_s].present?) ? p.title[I18n.locale.to_s] : p.name, p.id]
     end
 
-    form.select :participatory_process_id, collection, {} , data: { 'load-scoped-fields-url': load_scoped_fields_moderation_meetings_path(format: :js), 'meeting-id': @meeting.id}
+    form.select :participatory_process_id, collection, {} , data: { 'load-scoped-fields-url': load_scoped_fields_moderation_meetings_path(format: :js), 'meeting-id': @meeting.try(:id)}
   end
 end
