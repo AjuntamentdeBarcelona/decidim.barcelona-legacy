59b4b3c2b8c7e4834fc50170e420bfca753bfc8a
David Morcillo <david.morcillo@gmail.com>
2016-10-19 16:13:38 +0200

    Implement new design for meetings show page (#561)
    * Implement new design for show page
    
    * Fix i18n issues

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -11,7 +11,12 @@ module IconsHelper
     html_properties["aria-label"] = options[:aria_label]
     html_properties["role"] = options[:role]
     html_properties["aria-hidden"] = options[:aria_hidden]
-    html_properties["class"] = "icon icon--#{name} #{options[:class]}"
+
+    if options[:removeIconClass]
+      html_properties["class"] = "#{options[:class]}"
+    else
+      html_properties["class"] = "icon icon--#{name} #{options[:class]}"
+    end
 
     content_tag :svg, html_properties do
       content_tag :use, nil, "xlink:href" => "#{asset_url("decidim-icons.svg")}#icon-#{name}"
