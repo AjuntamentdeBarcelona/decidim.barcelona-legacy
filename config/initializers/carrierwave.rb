a842ab94d42b9e054effb3ddb24ee7a1f469f655
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-10-20 12:20:31 +0200

    Add aws host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      host:                  Rails.application.secrets.aws_host,
       region:                Rails.application.secrets.aws_region,
     }
 
