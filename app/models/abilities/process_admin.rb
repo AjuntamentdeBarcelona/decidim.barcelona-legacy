5d82b354e1c51d4f14d8a97ba4fe628bc30aeaaa
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2016-10-20 14:25:02 +0200

    Expand process admin abilities

--- app/models/abilities/process_admin.rb
+++ app/models/abilities/process_admin.rb
@@ -3,12 +3,18 @@ module Abilities
     include CanCan::Ability
 
     def initialize(user)
+      self.merge Abilities::Common.new(user)
+
+      can :access_panel, :moderation
       can :access_panel, :administration
       can :manage, ParticipatoryProcessAttachment
       can :manage, Category
       can :manage, Subcategory
       can :manage, ParticipatoryProcess
       can :manage, Step
+
+      can :manage, Meeting
+      can :create, Meeting
     end
   end
 end
