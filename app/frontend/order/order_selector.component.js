8e7938388f5243153a58054a71ea7f92d86ef8ca
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-10-25 09:51:20 +0200

    Use new order selector component

--- app/frontend/order/order_selector.component.js
+++ app/frontend/order/order_selector.component.js
@@ -4,24 +4,38 @@ import { connect }   from 'react-redux';
 import * as actions  from '../order/order.actions';
 
 class OrderSelector extends Component {
+  componentDidMount() {
+    $('.dropdown').foundation();
+  }
+
   render() {
     const { orderLinks, order, setOrder } = this.props;
 
     return (
-      <ul className="menu">
-        {
-          orderLinks.map(orderLink => {
-            return (
-              <li key={orderLink} className={order === orderLink ? `${orderLink} active` : orderLink}>
-                <a onClick={() => setOrder(orderLink)} >
-                  { I18n.t(`components.order_selector.${orderLink}`) }
-                </a>
-              </li>
-            );
-          })
-        }
-      </ul>
-    );
+      <div className="row collapse order-by">
+        <div className="order-by__dropdown">
+          <span className="order-by__text">{ I18n.t('components.order_selector.title') }:</span>
+          <ul className="dropdown menu" data-dropdown-menu="">
+            <li>
+              <a>{ I18n.t(`components.order_selector.${order}`) }</a>
+              <ul className="menu">
+                {
+                  orderLinks.map(orderLink => {
+                    return (
+                      <li key={orderLink}>
+                        <a onClick={() => setOrder(orderLink)}>
+                          { I18n.t(`components.order_selector.${orderLink}`) }
+                        </a>
+                      </li>
+                    );
+                  })
+                }
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );``
   }
 }
 
