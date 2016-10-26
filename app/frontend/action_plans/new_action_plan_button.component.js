ec7cfc39d84a6b607c2e8d133117d2f10aa2315d
David Morcillo <david.morcillo@gmail.com>
2016-10-26 12:18:38 +0200

    Add new design to new action plan button (#666)

--- app/frontend/action_plans/new_action_plan_button.component.js
+++ app/frontend/action_plans/new_action_plan_button.component.js
@@ -1,13 +1,17 @@
 import { Component, PropTypes } from 'react';
 import { connect }              from 'react-redux';
 
+import Icon                     from '../application/icon.component';
+
 class NewActionPlanButton extends Component {
   render() {
     const { session, participatoryProcessId, stepId } = this.props;
 
     if (session.can_create_action_plan) {
       return (
-        <a href={`/${participatoryProcessId}/${stepId}/action_plans/new`} className="new-proposal button radius expand">{I18n.t("action_plans.index.new")}</a>
+        <a href={`/${participatoryProcessId}/${stepId}/action_plans/new`} className="new-proposal title-action__action button small hollow">{I18n.t("action_plans.index.new") + ' '}
+          <Icon name="plus"/>
+        </a>
       );
     }
     return null;
