c8e174f46ac37e773137850568a65a632e854baf
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-10-26 09:45:42 +0200

    Remove unnecessary div

--- app/frontend/action_plans/action_plans_sidebar.component.js
+++ app/frontend/action_plans/action_plans_sidebar.component.js
@@ -1,11 +1,9 @@
 import ActionPlansFilters  from './action_plans_filters.component';
 
 const ActionPlansSidebar = () => (
-  <div className="filters-controls">
-    <div className="card card--secondary show-for-mediumlarge">
-      <div className="filters">
-        <ActionPlansFilters />
-      </div>
+  <div className="card card--secondary show-for-mediumlarge">
+    <div className="filters">
+      <ActionPlansFilters />
     </div>
   </div>
 );
