3c266912dd52edb9e862634eee785d7473183c07
David Morcillo <david.morcillo@gmail.com>
2016-10-26 10:19:53 +0200

    Remove unnecessary div (#668)

--- app/frontend/proposals/proposals_sidebar.component.js
+++ app/frontend/proposals/proposals_sidebar.component.js
@@ -1,11 +1,9 @@
 import ProposalsFilters  from './proposals_filters.component';
 
 const ProposalsSidebar = () => (
-  <div className="filters-controls">
-    <div className="card card--secondary show-for-mediumlarge">
-      <div className="filters">
-        <ProposalsFilters />
-      </div>
+  <div className="card card--secondary show-for-mediumlarge">
+    <div className="filters">
+      <ProposalsFilters />
     </div>
   </div>
 );
