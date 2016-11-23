8b19cb45a6baec22f3d6d0ae080efe7bc3d93db2
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-11-23 13:49:10 +0100

    Handle no features scenario for step path

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,6 +1,7 @@
 module StepsHelper
   def feature_path(step)
     feature = [:proposals, :action_plans, :meetings, :debates, :dataviz, :categories].find { |f| step.feature_enabled?(f) }
+    return step_path(participatory_process_id: step.participatory_process.slug, step_id: step) unless feature
     send("#{feature}_path", participatory_process_id: step.participatory_process.slug, step_id: step.id)
   end
 end
