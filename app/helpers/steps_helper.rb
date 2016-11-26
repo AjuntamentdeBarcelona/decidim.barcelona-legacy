cb6141fd0fafd9909d80a8de55b140d7caf1dfb0
David Morcillo Muñoz <david.morcillo@gmail.com>
2016-11-26 21:33:06 +0100

    Fix related proposals link from meeting page

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -4,4 +4,11 @@ module StepsHelper
     return step_path(participatory_process_id: step.participatory_process.slug, step_id: step) unless feature
     send("#{feature}_path", participatory_process_id: step.participatory_process.slug, step_id: step.id)
   end
+
+  def active_step_for_feature(object, feature_name)
+    object.participatory_process
+      .includes(:steps)
+      .steps.where(active: true).order('position desc')
+      .to_a.find{ |s| s.flags.include? feature_name }
+  end
 end
